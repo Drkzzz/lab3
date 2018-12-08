1515b8d43110e50b457bf5d197f3458b7bd04aa7
Drkzzz <joaquin.escobarr@gmail.com>
2018-12-08 02:47:50 -0300

    píco

---
+++ lab.c
@@ -0,0 +1,280 @@
+/*
+    *Nombres:Joaquin Escobar , Yerko Tolosa
+    *Asignatura: Fundamentos de Programacion
+    *Profesores:Irene Zuccar , Marcelo Baeza
+    *Fecha entrega:Domingo 25 de noviembre 2018
+*/
+
+
+/*Headers*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "funcionesParaImprimir.h"
+/*FIN*/
+
+/*Macros propios*/
+#define salto printf("\n")
+#define and &&
+#define or ||
+/*FIN*/
+
+int ttablero,nembarcaciones,ttablero,tdisponibles,c; int tablero[N][N];
+int p_Ganadas=0,p_Perdidas=0;
+char f;
+
+void fArchivo(void)
+{
+
+FILE *archivo;
+
+char nombre_archivo[100]; int ciclo=0,i,j;
+int num1,num2,num3;
+
+/*Ciclo que abre nuestro archivo si el texto coincide con el nombre "tablero1.txt"*/
+    printf("Ingrese nombre archivo: ");
+	while(ciclo==0)
+	{
+        scanf("%s", nombre_archivo);
+        archivo = fopen(nombre_archivo,"r");
+
+            if(archivo==NULL)
+            {
+                printf("Ingrese nombre archivo: ");
+            }
+            else
+            {
+                break;
+            }
+
+	}
+/*FIN*/
+
+/*Obtenemos nuestros datos*/
+    /*Tamaño tablero*/
+    fscanf(archivo,"%d", &num1);
+    /*Numero Embarcaciones*/
+    fscanf(archivo,"%d", &num2);
+    /*Datos para matriz*/
+    char linea[num1+1];
+    for(i = 0; i < num1; i++)
+    {
+        fscanf(archivo, "%s",linea);
+        for( j = 0 ; j < num1; j++)
+        {
+            tablero[i][j] = linea[j];
+        }
+    }
+    /*Tiros disponibles*/
+    fscanf(archivo, "%d", &num3);
+/*FIN*/
+
+/*Asignamos valores de nuestras variables auxiliares*/
+ttablero=num1;
+nembarcaciones=num2;
+tdisponibles=num3;
+/*FIN*/
+
+/*Cerramos el archivo*/
+fclose(archivo);
+/*FIN*/
+
+LimpiaPantalla();
+
+}
+
+void fIngresaFyC(void)
+{
+
+int ciclo=0;
+
+	/*Se solicitan ingresos de los datos*/
+	printf("Ingresa Fila: ");
+    while(ciclo==0)
+    {
+
+        scanf(" %c",&f);
+		fflush(stdin);
+		/*Si es minuscula lo convierte con la libreria ctype.h*/
+		if( islower(f) )
+        {
+            f = (toupper(f));
+        }
+		/*FIN*/
+        if( (f-64>ttablero)or(!(f>='A'&&f<='Z')) )
+        {
+            printf("Ingresa Fila: ");
+        }
+        else
+        {
+            f=f-65;
+            break;
+        }
+
+    }
+	printf("Ingresa Columna: ");
+    while(ciclo==0)
+    {
+
+        scanf("%d",&c);
+        fflush(stdin);
+        if( (c<1)or(c>ttablero) )
+		{
+			printf("Ingresa Columna: ");
+		}
+        else
+        {
+            c=c-1;
+            break;
+        }
+
+    }
+   /*FIN*/
+
+}
+
+void fValidaPos(void)
+{
+
+	/*Validamos si ya hemos ocupado la posicion anteriormente*/
+    do
+    {
+
+        fIngresaFyC();
+		salto;
+            if( (tablero[f][c]=='X')or(tablero[f][c]=='D') )
+            {
+                printf("Ya tiraste ahi!");
+                salto;
+            }
+
+    } while( (tablero[f][c]=='X')or(tablero[f][c]=='D') );
+    /*FIN*/
+
+}
+void fJuego(void)
+{
+
+int ciclo;
+ciclo=0;
+
+    /*Comienza el juego*/
+	ImprimeTablero(tablero,ttablero); salto; salto;
+    while(tdisponibles>0)
+    {
+
+        printf("Total barcos: %d - Tiros Disponibles: %d",nembarcaciones,tdisponibles); salto; salto;
+		/*Identificamos si en la cordenada hay una nave o no*/
+
+		fValidaPos();
+
+            if( tablero[f][c]=='N' )
+            {
+                tablero[f][c]='D';
+            }
+            else if ( tablero[f][c]=='A')
+            {
+                tablero[f][c]='X';
+            }
+		/*FIN*/
+
+		/*Imprimimos tablero actualizado y nos queda un tiro menos*/
+		LimpiaPantalla();ImprimeTablero(tablero,ttablero);salto;salto;
+		tdisponibles--;
+		/*FIN*/
+
+    }
+
+	LimpiaPantalla();
+
+}
+
+void fRevisiontablero(void)
+{
+
+int i,j,count,ciclo;
+char opcion;
+count=0;
+ciclo=0;
+
+	/*Chequeamos pos por pos*/
+	for(i=0;i<ttablero;i++)
+	{
+		for(j=0;j<ttablero;j++)
+		{
+			if(tablero[i][j]=='N')
+			{
+			count++;
+			}
+		}
+	}
+	/*FIN*/
+
+	/*Termina el juego*/
+	if(count==0)
+    {
+		LimpiaPantalla();
+        Ganaste();
+        p_Ganadas++;
+    }
+    else
+    {
+		LimpiaPantalla();
+        Perdiste();
+        p_Perdidas++;
+    }
+    	/*Se muestran resultados de partidas*/
+    	salto;salto;
+    	printf("Has ganado %d partida.", p_Ganadas);salto;
+    	printf("Has perdido %d partida.", p_Perdidas);salto;
+    	salto;
+		/*FIN*/
+
+    /*FIN*/
+
+	/*Ciclo que pregunta si quiere reiniciar juego*/
+	printf("Quieres volver a jugar (S/N)?: ");
+    while( ciclo==0 )
+    {
+        
+        scanf(" %c", &opcion);
+	    fflush(stdin);
+            /*Si es minuscula lo convierte con la libreria ctype.h*/
+            if( islower(opcion) )
+            {
+                opcion= (toupper(opcion));
+            }
+            /*FIN*/
+            if( !(opcion=='S'or opcion=='N') )
+            {
+                printf("Quieres volver a jugar (S/N)?: ");
+            }
+            else
+            {
+                break;
+            }
+
+    }
+    /*FIN*/
+
+	/*Si el usuario ingresa S reinicia el juego*/
+    if(opcion=='S')
+    {
+        LimpiaPantalla();
+        fArchivo();
+        fJuego();
+        fRevisiontablero();
+    }
+	/*FIN*/
+
+}
+
+void main(void)
+{
+
+	fArchivo();
+	fJuego();
+	fRevisiontablero();
+
+}
+
